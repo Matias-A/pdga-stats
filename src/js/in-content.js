ba8fb16f2984855e78c9d955fd944640d398ef24
Matias-A <matiaskb.arola@gmail.com>
2021-04-19 12:33:43 -0700

    Extended jQuery functionality and used the acquired data to populate the table. First working version.

--- src/js/in-content.js
+++ src/js/in-content.js
@@ -2,7 +2,7 @@ let currUrl = location.href;
 let numRe = /.*\/player\/(\d+)(?:\/|$).*/
 let match = currUrl.match(numRe);
 // TODO: figure out if this is a stable way to find the PDGA number
-let pdgaNumber = match[1];
+const pdgaNumber = match[1];
 
 let tablesLoaded = false;
 let extraCols = [
@@ -26,7 +26,6 @@ document.addEventListener('readystatechange', (event) => {
 
     for (let i = 0; i < tables.length; i++) {
         let table = tables[i];
-        console.log(table);
 
         let thead = table.getElementsByTagName("thead");
         let tbody = table.getElementsByTagName("tbody");
@@ -72,16 +71,50 @@ function fetchData(bodyRows) {
         let tournamentLink = tournamentCell.getElementsByTagName("a")[0];
         // Fetches the web page for this tournament using jQuery
         $.get(tournamentLink, (page) => {
-            $("table.results", page).each((index, table) => {
-                console.log($(table).html());
+            $("table.results", page).each((_, table) => {
+                let numRows = $("tr", table).length-1;
+                $("tr", table).each((_, playerRow) => {
+                    let num = $("td.pdga-number", playerRow).text();
+                    if (num === pdgaNumber) {
+                        // This means that the current table is the correct division
+                        // And the currect row corresponds to the correct player
+                        let numRounds = $("td.round-rating", playerRow).length;
+                        let numProcessed = 0;
+                        let ratings = [];
+                        $("td.round-rating", playerRow).each((_, cell) => {
+                            let rating = $(cell).text();
+                            if (rating) {
+                                rating = parseInt(rating);
+                                ratings.push(rating);
+                            }
+                            numProcessed += 1;
+                            if (numProcessed === numRounds) {
+                                updateData(row, {
+                                    ratings: ratings,
+                                });
+                            }
+                        });
+                    }
+                })
+                //console.log($(table).html());
             });
         });
-        break; // TODO: delete this row
+        //break; // TODO: delete this row
     }
 }
 
 // Takes the relevant data found in fetchData,
 // updates the newly added columns in this specific row
 function updateData(row, data) {
-    // TODO: fill this in
+    const roundRatings = data.ratings;
+    const numRounds = roundRatings.length;
+    const ratingSum = roundRatings.reduce((a, b) => a + b, 0);
+    const ratingAvg = (ratingSum / numRounds) || 0;
+
+    // Average rating (hard coded for now)
+    let ratingAvgCell = row.getElementsByClassName("average-round-rating")[0];
+    ratingAvgCell.innerHTML = Math.round(ratingAvg);
+    // Num of rounds (hard coded for now)
+    let numRoundsCell = row.getElementsByClassName("number-of-rounds")[0];
+    numRoundsCell.innerHTML = numRounds;
 }
