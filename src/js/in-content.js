dbb36547fa76c99882e95b93b8065a080c765d18
Matias-A <matiaskb.arola@gmail.com>
2021-04-18 20:49:13 -0700

    Fixed the earlier issue by creating a readystatechange event listener. Now correctly updates the table with dummy data.

--- src/js/in-content.js
+++ src/js/in-content.js
@@ -1,25 +1,44 @@
-let elems = document.getElementsByClassName("pane-page-title");
-if (elems[0] !== undefined) {
-    let elemdiv1 = elems[0];
-    let elemdiv2 = elemdiv1.getElementsByClassName("pane-content")[0];
-    if (elemdiv2 !== undefined) {
-        let text = elemdiv2.getElementsByTagName("h1")[0];
-        text.innerHTML += " (extension working)";
+let tablesLoaded = false;
+let extraCols = [
+    {
+        "name" : "# Rounds"
     }
-}
+]
 
-let tables = document.getElementsByClassName("tablesorter");
-console.log(tables);
-console.log(tables.length);
-for (let i = 0; i < tables.length; i++) {
-    let table = tables[i];
+document.addEventListener('readystatechange', (event) => {
+    // Prevents infinite loops when new data is loaded
+    if (tablesLoaded) return;
 
-    console.log(table);
-    let rows = table.getElementsByTagName("tr");
-    for (let j = 0; j < rows.length; j++) {
-        let row = rows[j];
-        let newCell = row.insertCell(-1);
-        newCell.innerHTML = "(empty)"
+    let tables = document.getElementsByClassName("tablesorter");
+    // Waits until the table is populated
+    if (tables.length === 0) return;
+
+    for (let i = 0; i < tables.length; i++) {
+        let table = tables[i];
+        console.log(table);
+
+        let thead = table.getElementsByTagName("thead");
+        let headerRows = thead[0].getElementsByTagName("tr");
+        let tbody = table.getElementsByTagName("tbody");
+        let bodyRows = tbody[0].getElementsByTagName("tr");
+
+        // Adds each of the extra columns and fetches the relevant data
+        for (let j=0; j<extraCols.length; j++) {
+            // First updates the column header
+            let columnHeader = document.createElement("TH");
+            let columnHeaderDiv = document.createElement("DIV");
+            columnHeaderDiv.innerHTML = extraCols[j].name;
+            columnHeaderDiv.className = "tablesorter-header-inner";
+            columnHeader.appendChild(columnHeaderDiv);
+            headerRows[0].appendChild(columnHeader);
+
+            for (let k=0; k<bodyRows.length; k++) {
+                let row = bodyRows[k];
+                let newCell = row.insertCell(-1);
+                newCell.innerHTML = "(loading...)"
+            };
+        }
     };
 
-};
+    tablesLoaded = true;
+});
